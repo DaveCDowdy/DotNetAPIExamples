5d8a5aaf1b3c8864a7aa730945d4cd4769b8f10d
DaveCDowdy <thedaviddowdy@gmail.com>
2024-08-28 23:40:58 -0500

    Added CreatedAtRoute

--- MagicVilla_VIllaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VIllaAPI/Controllers/VillaAPIController.cs
@@ -17,7 +17,7 @@ namespace MagicVilla_VIllaAPI.Controllers
             return Ok(VillaStore.villaList);
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name = "GetVilla")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
@@ -36,7 +36,7 @@ namespace MagicVilla_VIllaAPI.Controllers
         }
 
         [HttpPost]
-        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<VillaDTO> CreateVilla([FromBody]VillaDTO villaDTO)
@@ -52,7 +52,7 @@ namespace MagicVilla_VIllaAPI.Controllers
             villaDTO.Id = VillaStore.villaList.OrderByDescending(villa => villa.Id).FirstOrDefault().Id + 1;
             VillaStore.villaList.Add(villaDTO);
 
-            return Ok(villaDTO);
+            return CreatedAtRoute("GetVilla",new { id = villaDTO.Id }, villaDTO);
         }
     }
 }
