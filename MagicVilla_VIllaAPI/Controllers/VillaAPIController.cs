e8eef82d5488dd379e057b39f5c9ab5e08fe03fc
DaveCDowdy <thedaviddowdy@gmail.com>
2024-08-28 20:00:13 -0500

    Added GetVIlla method to controller

--- MagicVilla_VIllaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VIllaAPI/Controllers/VillaAPIController.cs
@@ -15,5 +15,11 @@ namespace MagicVilla_VIllaAPI.Controllers
         {
             return VillaStore.villaList;
         }
+
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(villa=> villa.Id==id);
+        }
     }
 }
