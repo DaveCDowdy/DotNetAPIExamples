97947215cf4ee0fa9d06538f5ccd2f5f473a891b
DaveCDowdy <thedaviddowdy@gmail.com>
2024-08-28 19:12:03 -0500

    Wired up first endpoint

---
+++ MagicVilla_VIllaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,21 @@
+﻿using MagicVilla_VIllaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace MagicVilla_VIllaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa { Id = 1, Name = "Pool View" },
+                new Villa { Id = 2, Name = "Beach View" }
+            };
+        }
+    }
+}
