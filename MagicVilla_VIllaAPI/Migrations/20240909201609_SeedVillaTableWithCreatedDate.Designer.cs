de98d45ff4238c6294321211d78f3cc0d2dea463
DaveCDowdy <thedaviddowdy@gmail.com>
2024-09-09 15:18:17 -0500

    Seeded Villas table through EF Core

--- MagicVilla_VIllaAPI/Migrations/20240909201609_SeedVillaTableWithCreatedDate.Designer.cs
+++ MagicVilla_VIllaAPI/Migrations/20240909201609_SeedVillaTableWithCreatedDate.Designer.cs
@@ -4,6 +4,7 @@ using MagicVilla_VIllaAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MagicVilla_VIllaAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240909201609_SeedVillaTableWithCreatedDate")]
+    partial class SeedVillaTableWithCreatedDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
