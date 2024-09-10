1118b5e7f6caaa71c7986da619dec048ee2399f2
DaveCDowdy <thedaviddowdy@gmail.com>
2024-09-10 10:52:04 -0500

    implemented repository

--- MagicVilla_VIllaAPI/Repository/IRepository/IVIllaRepository.cs
+++ MagicVilla_VIllaAPI/Repository/IRepository/IVIllaRepository.cs
@@ -3,10 +3,10 @@ using System.Linq.Expressions;
 
 namespace MagicVilla_VIllaAPI.Repository.IRepository
 {
-    public interface IVIllaRepository
+    public interface IVillaRepository
     {
-        Task<List<Villa>> GetAll(Expression<Func<Villa>> filter = null);
-        Task<Villa> Get(Expression<Func<Villa>> filter = null, bool tracked = true);
+        Task<List<Villa>> GetAll(Expression<Func<Villa, bool>> filter = null);
+        Task<Villa> Get(Expression<Func<Villa, bool>> filter = null, bool tracked = true);
         Task Create(Villa entity);
         Task Remove(Villa entity);
         Task Save();
